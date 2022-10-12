dbd40f6e834bc5537184dd5c8ad2b3f2d4b3b17c
Mugenya <bmugenya26@gmail.com>
2022-10-12 22:57:28 +0300

    uniswap widget

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,77 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
+
+import { providers, ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
+import {SwapWidget } from '@uniswap/widgets'
+
+
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
 import icon from '../images/cryptocurrency.png';
+// import { Modal } from '@mui/material';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+const infuraId = '6b1c27a439bc4a09b6cedfd1d501c07d';
+const JsonRpcEndpoint = `https://mainnet.infura.io/v3/${infuraId}`;
+const JsonRpcProvider = new providers.JsonRpcProvider(JsonRpcEndpoint);
+const provider = new ethers.providers.Web3Provider(JsonRpcProvider);
+
+
+
+const jsonRpcUrlMap = { 
+  1: ['https://mainnet.infura.io/v3/6b1c27a439bc4a09b6cedfd1d501c07d>'], 
+  3: ['https://ropsten.infura.io/v3/6b1c27a439bc4a09b6cedfd1d501c07d']
+}
+
+
 
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [screenSize, setScreenSize] = useState(undefined);
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  const [account, setAccount] = useState({
+    address: '',
+    provider: provider
+  })
+
+  console.log(account)
+
+  async function connectWallet() {
+    const ethereumProvider = await detectEthereumProvider();
+
+    if (ethereumProvider) {
+      const address = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+      })
+      setAccount({
+        address: address[0],
+        provider: ethereumProvider
+      })
+    }
+  }
+
+
 
   const [data, setdata] = useState({
     address: '',
@@ -46,9 +112,9 @@ const Navbar = () => {
       })
       .then((balance) => {
         // Setting balance
-        setdata({
-          Balance: ethers.utils.formatEther(balance),
-        });
+        // setdata({
+        //   Balance: ethers.utils.formatEther(balance),
+        // });
       });
   };
   useEffect(() => {
@@ -66,6 +132,25 @@ const Navbar = () => {
   }, [screenSize]);
   return (
     <div className="nav-container">
+      <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+    <button onClick={() => connectWallet()}>Connect Wallet</button>
+    </Typography>
+  
+      <SwapWidget provider={account.provider} jsonRpcUrlMap={jsonRpcUrlMap} />
+
+
+  </Box>
+</Modal>
+
+
+
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo"><Link to="/">Cryptoverse</Link></Typography.Title>
@@ -80,7 +165,7 @@ const Navbar = () => {
           <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </Menu.Item>
         <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
+          <span onClick={handleOpen}>Exchanges</span>
         </Menu.Item>
         <Menu.Item icon={<BulbOutlined />}>
           <Link to="/news">News</Link>
